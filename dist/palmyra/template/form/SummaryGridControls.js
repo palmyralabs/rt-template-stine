a512e0f97deec709913e096407ccd2b7d7fd2dd9
Agasthiya1803 <cs1803agasthiyar@gmail.com>
2024-09-20 13:04:36 +0530

    latest build

--- dist/palmyra/template/form/SummaryGridControls.js
+++ dist/palmyra/template/form/SummaryGridControls.js
@@ -1,33 +1,37 @@
-import { jsxs as s, Fragment as u, jsx as o } from "react/jsx-runtime";
-import { QuickSearch as l, FilterButton as a, ExportDataButton as d } from "@palmyralabs/rt-forms-mui";
-import { Button as m } from "@mui/material";
-const h = (r) => {
+import { jsxs as s, Fragment as u, jsx as r } from "react/jsx-runtime";
+import { QuickSearch as d, FilterButton as l, ExportDataButton as m } from "@palmyralabs/rt-forms-mui";
+import { Button as a } from "@mui/material";
+import { Add as p } from "@mui/icons-material";
+const y = (e) => {
   var n, c;
-  const { getPluginOptions: i, ...t } = r, e = i ? i() : {};
+  const { getPluginOptions: i, ...t } = e, o = i ? i() : {};
   return /* @__PURE__ */ s(u, { children: [
-    t.quickSearch && /* @__PURE__ */ o(
-      l,
+    t.quickSearch && /* @__PURE__ */ r(
+      d,
       {
         width: "200",
         queryRef: t.queryRef,
         columns: t.columns,
-        ...e.quickSearch
+        ...o.quickSearch
       }
     ),
-    /* @__PURE__ */ o(a, { ...t }),
-    /* @__PURE__ */ o(
-      d,
+    /* @__PURE__ */ r(l, { ...t }),
+    /* @__PURE__ */ r(
+      m,
       {
         exportOption: { csv: "CSV" },
-        visible: (n = e.export) == null ? void 0 : n.visible,
-        disabled: (c = e.export) == null ? void 0 : c.disabled,
+        visible: (n = o.export) == null ? void 0 : n.visible,
+        disabled: (c = o.export) == null ? void 0 : c.disabled,
         queryRef: t.queryRef,
-        ...e.export
+        ...o.export
       }
     ),
-    /* @__PURE__ */ o(m, { onClick: () => r.newRecord(), ...e.add, className: "py-action-button", children: "Add" })
+    /* @__PURE__ */ s(a, { onClick: () => e.newRecord(), ...o.add, className: "py-action-button", children: [
+      /* @__PURE__ */ r(p, {}),
+      "Add"
+    ] })
   ] });
 };
 export {
-  h as SummaryGridControls
+  y as SummaryGridControls
 };
